af8244cdec0e3c56f9d892bb113da8104b359157
zwping <1101558280@qq.com>
2019-06-10 17:32:43 +0800

    debug ing

---
+++ src/main/java/win/zwping/code/cview/pi/IBadgeLayout.java
@@ -0,0 +1,23 @@
+package win.zwping.code.cview.pi;
+
+import android.view.View;
+
+import androidx.annotation.LayoutRes;
+import androidx.annotation.Nullable;
+
+import win.zwping.code.cview.BadgeLayout;
+
+public interface IBadgeLayout {
+
+//    BadgeLayout setBadgeView(View view);
+//
+//    BadgeLayout setBadgeView(@LayoutRes int id);
+//
+//    BadgeLayout setBadgeViewWH(int wDp, int hDp);
+//
+//    BadgeLayout setBadgeViewWH(int wDp);
+//
+//    @Nullable
+//    View getBadgeView();
+
+}
